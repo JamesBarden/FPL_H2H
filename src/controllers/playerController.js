6ea183025cd834aa684cbb3e8b027f455ec33fab
jamesbarden <barden.j@northeastern.edu>
2024-03-18 13:17:13 -0400

    add playerController

---
+++ src/controllers/playerController.js
@@ -0,0 +1,18 @@
+const Player = require('../models/Player');
+
+function updatePlayerPoints(playerName, gameweek, points) {
+    Player.updateOne(
+        { name: playerName },
+        { $set: { [`gameweekPoints.${gameweek - 1}`]: points } },
+        (err, result) => {
+            if (err) {
+                console.error('Error updating points for player', playerName, ':', err);
+            } else {
+                console.log('Points updated for player', playerName);
+            }
+        }
+    );
+}
+
+// Example usage
+updatePlayerPoints('Kevin De Bruyne', 1, 5);
